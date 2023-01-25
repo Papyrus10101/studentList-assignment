8801cabc253c15d209480cd02c3efe418f373ab0
Noah Nunez <noah10504@yahoo.ca>
2023-01-24 20:21:31 -0800

    student results

---
+++ studentList.js
@@ -0,0 +1,13 @@
+let student = [ 
+    { name: "Daniel", email: "daniel@gmail.com", marks: [80, 60, 50, 70, 95] },
+
+    { name: "Mark", email: "mark@gmail.com", marks: [99, 40, 84, 72, 60] },
+
+
+    { name: "Stacy", email: "stacy@gmail.com", marks: [8, 30, 11, 0, 20] },
+
+    { name: "Geri", email: "geri@gmail.com", marks: [100, 99, 95, 85, 99] }
+];
+
+
+
